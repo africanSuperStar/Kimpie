41e519312215f5da3d1faa29b90975b2e8557635
Cameron de Bruyn <boertjie.seun@outlook.com>
2021-06-04 20:26:14 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "KMM_App_Builder"
+include(":android")
+include(":shared")
---
+++ shared/src/commonMain/kotlin/ai/hinton/kmmappbuilder/Greeting.kt
@@ -0,0 +1,7 @@
+package ai.hinton.kmmappbuilder
+
+class Greeting {
+    fun greeting(): String {
+        return "Hello, ${Platform().platform}!"
+    }
+}
